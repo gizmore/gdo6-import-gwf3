67007db5d1cac81385bdc4955884f211d7869d2e
gizmore <gizmore@wechall.net>
2017-10-05 20:31:30 +0200

    6.04_fixes_httputil_moved_to_net

---
+++ Method/ImportAvatars.php
@@ -0,0 +1,14 @@
+<?php
+namespace GDO\ImportGWF3\Method;
+use GDO\ImportGWF3\MethodImport;
+/**
+ * @author gizmore
+ */
+final class ImportUsers extends MethodImport
+{
+    public function run()
+    {
+        
+    }
+    
+}
--- Module_ImportGWF3.php
+++ Module_ImportGWF3.php
@@ -3,6 +3,7 @@ namespace GDO\ImportGWF3;
 use GDO\Core\GDO_Module;
 use GDO\Net\GDT_Hostname;
 use GDO\Type\GDT_Secret;
+use GDO\File\GDT_Path;
 /**
  * Import gwf3 tables into this gdo6 installation.
  * 
@@ -13,8 +14,12 @@ use GDO\Type\GDT_Secret;
 final class Module_ImportGWF3 extends GDO_Module
 {
     public function defaultEnabled() { return false; }
+    
     public function href_administrate_module() { return href('ImportGWF3', 'Admin'); }
     
+    ##############
+    ### Config ###
+    ##############
     public function getConfig()
     {
         return array(
@@ -23,12 +28,13 @@ final class Module_ImportGWF3 extends GDO_Module
             GDT_Secret::make('gwf3_db_pass')->initial('wechall5'),
             GDT_Secret::make('gwf3_db_name')->initial('wechall5'),
             GDT_Secret::make('gwf3_db_prefix')->initial('wc4_'),
+            GDT_Path::make('gwf3_dbimg_dir')->existingDir(),
         );
     }
-    
     public function cfgDBHost() { return $this->getConfigVar('gwf3_db_host'); }
     public function cfgDBUser() { return $this->getConfigVar('gwf3_db_user'); }
     public function cfgDBPass() { return $this->getConfigVar('gwf3_db_pass'); }
     public function cfgDBName() { return $this->getConfigVar('gwf3_db_name'); }
     public function cfgDBPrefix() { return $this->getConfigVar('gwf3_db_prefix'); }
+    public function cfgGWF3Path() { return $this->getConfigVar('gwf3_dbimg_dir'); }
 }
